56650ce612c952c9fa1f20dfabcbec45149364f1
Kamal <31344909+kmldpsng@users.noreply.github.com>
2021-07-19 00:06:25 +0530

    making delay and increment dynamic

--- GreatProject.UI/src/Organism/gallery.js
+++ GreatProject.UI/src/Organism/gallery.js
@@ -1,27 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import PICTURES from '../data/pictures';
 
+const minimumDelay = 1;
+const minimumIncrement = 1;
 export default function Gallery() {
 
-    const [delay, setDelay] = useState(3000);
+    const [delay, setDelay] = useState(minimumDelay * 1000);
     const [index, setIndex] = useState(1);
+    const [increment, setIncrement] = useState(minimumIncrement);
+
+    const updateDelayTimer = (event => {
+        var userDefinedDelay = Number(event.target.value);
+        setDelay((userDefinedDelay < minimumDelay) ? (minimumDelay * 1000) : (userDefinedDelay * 1000));
+    });
+
+    const updateIncrements = (event => {
+        var userDefinedIncrement = Number(event.target.value);
+        setIncrement((userDefinedIncrement < minimumIncrement) ? minimumIncrement : userDefinedIncrement);
+    });
 
     useEffect(() => {
         const interval = setInterval(() => {
             setIndex(storedIndex => {
-                return (storedIndex + 1) % PICTURES.length;
+                return (storedIndex + increment) % PICTURES.length;
             })
-        }, 1000);
+        }, delay);
 
-        return (() =>{ 
-            clearInterval(interval) 
+        return (() => {
+            clearInterval(interval)
         })
-    }, []);
+    }, [delay,increment]);
 
     return (
+
         <div className='Gallery'>
             <img src={PICTURES[index].image} alt="displaying"></img>
-        </div>
 
+            <div>
+                Gallery Delay Transition (seconds) :
+                <input type='Number' onChange={updateDelayTimer} />
+            </div>
+            <div>
+                Increment :
+                <input type='Number' onChange={updateIncrements} />
+            </div>
+
+        </div>
     )
 }
